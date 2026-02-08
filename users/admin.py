db0dbc408d56b76d322349a85476aeaf27d58640
Bekarys2207A <bekarysabdumutalip@gmail.com>
2026-02-09 00:57:33 +0500

    Feature 02: Add custom User model, TeacherProfile, RBAC

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, TeacherProfile
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'role', 'is_active', 'created_at')
+    list_filter = ('role', 'is_active')
+    search_fields = ('email',)
+
+@admin.register(TeacherProfile)
+class TeacherProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating')
+    search_fields = ('user__email',)
